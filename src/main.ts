5e41ca9ab989cc495f3e2cf24e9ff99f9048649b
Manuk03 <manuk.nersisyan.96@ifixgroup.com>
2022-06-16 17:17:10 +0400

    feat: install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule, { cors: true });
+  
+  const config = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .addBearerAuth()
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  
   await app.listen(3000, () => {
     console.log('server has been started');
   });
